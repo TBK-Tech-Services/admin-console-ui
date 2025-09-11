de270c32b8d41d4d837ebd75c875b3b8f0145e8d
sahil ladhania <sahilladhania@sahils-MacBook-Air.local>
2025-09-11 19:10:24 +0530

    feat: conditionally render User Management Section

--- tbk-admin/src/pages/Settings.tsx
+++ tbk-admin/src/pages/Settings.tsx
@@ -2,32 +2,43 @@ import GeneralSettings from "@/components/GeneralSettings";
 import SecurityAndBackupSettings from "@/components/SecurityAndBackupSettings";
 import UserManagementSettings from "@/components/UserManagementSettings";
 import VillaManagementSettings from "@/components/VillaManagementSettings";
+import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 export default function Settings() {
-    return (
-      <div className="space-y-6 max-w-4xl">
-        <div>
-          <h1 className="text-3xl font-bold text-foreground mb-2">
-            Settings
-          </h1>
-          <p className="text-muted-foreground">
-            Manage your villa booking system preferences and configurations.
-          </p>
+      // useSelector
+      const user = useSelector((state: RootState) => state.auth.user);
+      const email = user?.email;
+      const userRole = user?.role;
+      console.log(userRole);
+      console.log(email);
+
+      return (
+        <div className="space-y-6 max-w-4xl">
+          <div>
+            <h1 className="text-3xl font-bold text-foreground mb-2">
+              Settings
+            </h1>
+            <p className="text-muted-foreground">
+              Manage your villa booking system preferences and configurations.
+            </p>
+          </div>
+
+          <div className="grid gap-6">
+            {/* General Settings */}
+            <GeneralSettings />
+
+            {/* Villa Management */}
+            <VillaManagementSettings />
+
+            {/* User Management */}
+            {
+              (userRole === "Admin") && <UserManagementSettings />
+            }
+
+            {/* Security */}
+            <SecurityAndBackupSettings />
+          </div>
         </div>
-
-        <div className="grid gap-6">
-          {/* General Settings */}
-          <GeneralSettings />
-
-          {/* Villa Management */}
-          <VillaManagementSettings />
-
-          {/* User Management */}
-          <UserManagementSettings />
-
-          {/* Security */}
-          <SecurityAndBackupSettings />
-        </div>
-      </div>
-    );
+      );
 }
